b97e8d8899955857814ecd4ba72790be47525d51
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2024-05-19 09:29:11 +0200

    Update Setup_CurseForge_Server.sh

--- Setup_CurseForge_Server.sh
+++ Setup_CurseForge_Server.sh
@@ -11,29 +11,26 @@ sudo ufw allow 25565/udp
 sudo ufw enable
 sudo ufw reload
 
-# Generate a random password for the minecraft user
-MC_PASSWORD=$(openssl rand -base64 12)
-echo "minecraft:$MC_PASSWORD" | sudo chpasswd
-echo "Minecraft user password: $MC_PASSWORD"
-
-# Create users for Minecraft
+# Create user for Minecraft without password
 sudo adduser --gecos "" --disabled-password minecraft
 
 # Create Minecraft directory and set permissions
 sudo mkdir /minecraft
 sudo chown -R minecraft:minecraft /minecraft
 
-# Download and install Minecraft server - Important Change URL here!
+# Download and install Minecraft server
 sudo -i -u minecraft bash << EOF
 cd /minecraft
 wget https://edge.forgecdn.net/files/5225/986/Server-Files-0.1.13.zip
 unzip Server-Files-0.1.13.zip
-cd BM_Exosphere_1.1.2_server_pack
-chmod +x start.sh
-./start.sh
+rm Server-Files-0.1.13.zip
+cd Server-Files-0.1.13
+echo "eula=true" > eula.txt
+chmod +x startserver.sh
+./startserver.sh
 EOF
 
-# Set up Minecraft service - Important Change Pfad here!
+# Set up Minecraft service
 sudo bash -c 'cat << EOF > /etc/systemd/system/minecraft.service
 [Unit]
 Description=Minecraft Server
@@ -41,8 +38,8 @@ After=network.target
 
 [Service]
 User=minecraft
-WorkingDirectory=/minecraft/BM_Exosphere_1.1.2_server_pack
-ExecStart=/usr/bin/screen -DmS minecraft /minecraft/BM_Exosphere_1.1.2_server_pack/startserver.sh
+WorkingDirectory=/minecraft/Server-Files-0.1.13
+ExecStart=/usr/bin/screen -DmS minecraft /minecraft/Server-Files-0.1.13/startserver.sh
 Restart=on-failure
 
 [Install]
@@ -58,7 +55,7 @@ cat << 'EOL' > /minecraft/backup.sh
 #!/bin/bash
 
 BACKUP_DIR="/minecraft/backups"
-SOURCE_DIR="/minecraft/BM_Exosphere_1.1.2_server_pack"
+SOURCE_DIR="/minecraft/Server-Files-0.1.13"
 TIMESTAMP=\$(date +"%F")
 BACKUP_FILE="\$BACKUP_DIR/minecraft_backup_\$TIMESTAMP.tar.gz"
 
@@ -73,7 +70,4 @@ EOF
 # Set up cron job for backup
 (crontab -l 2>/dev/null; echo "0 2 * * * /minecraft/backup.sh") | crontab -
 
-# Output next steps to user
-echo "Minecraft server setup is complete. Use the following command to connect to the Minecraft server console:"
-echo "sudo -i -u minecraft screen -r minecraft"
-echo "Once connected, accept the EULA by editing eula.txt in the server directory."
+echo "Minecraft server setup is complete and running."
